b1d24d458f89fcef983458ac6c661e9c1d7b8b27
rodolf2 <rodolfojrxgt@gmail.com>
2026-01-30 21:55:31 +0800

    Trying to fix login issue

--- src/index.js
+++ src/index.js
@@ -182,7 +182,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === "production" ? "lax" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
   }),
